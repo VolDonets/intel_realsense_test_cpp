b719f6250e4f89dbebd78ce9edd39de3abe52bd4
VolDonets <vovan.s.marsa@gmail.com>
2020-10-26 17:32:07 +0200

    Testing capturing.

--- rs-capture-main.cpp
+++ rs-capture-main.cpp
@@ -30,33 +30,36 @@ int main(int argc, char * argv[]) try
     // The default video configuration contains Depth and Color streams
     // If a device is capable to stream IMU data, both Gyro and Accelerometer are enabled by default
     pipe.start();
-    //cv::namedWindow("win1", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("win1", cv::WINDOW_AUTOSIZE);
 
-    int counter = 10;
-    while (counter-- > 0) // Application still alive?
+    for (int counter = 0; counter < 1000000; counter++) // Application still alive?
     {
+        std::cout << "counter: " << counter << "\n";
         rs2::frameset data = pipe.wait_for_frames().    // Wait for next set of frames from the camera
                 apply_filter(printer).     // Print each enabled stream frame rate
                 apply_filter(color_map);   // Find and colorize the depth data
+        std::cout << "counter OR?\n\n";
 
         // The show method, when applied on frameset, break it to frames and upload each frame into a gl textures
         // Each texture is displayed on different viewport according to it's stream unique id
-        rs2::video_frame rs_frame_color = data.get_color_frame();
-        std::cout << "frame info -> width: " << rs_frame_color.get_width() << ", height: " << rs_frame_color.get_height() << "\n";
+        //rs2::video_frame rs_frame_color = data.get_color_frame();
+        //std::cout << "frame info -> width: " << rs_frame_color.get_width() << ", height: " << rs_frame_color.get_height() << "\n";
         //std::cout << data.get_timestamp() << "\n";
-        cv::Mat cv_frame_color(cv::Size(rs_frame_color.get_width(), rs_frame_color.get_height()), CV_8UC3, (void*) rs_frame_color.get_data(), cv::Mat::AUTO_STEP);
-        //cv::imshow("win1", cv_frame);
+        //cv::Mat cv_frame_color(cv::Size(rs_frame_color.get_width(), rs_frame_color.get_height()), CV_8UC3, (void*) rs_frame_color.get_data(), cv::Mat::AUTO_STEP);
 
         rs2::depth_frame rs_frame_depth = data.get_depth_frame();
         cv::Mat cv_frame_depth(cv::Size(rs_frame_depth.get_width(), rs_frame_depth.get_height()), CV_8UC3, (void*) rs_frame_depth.get_data(), cv::Mat::AUTO_STEP);
 
-        std::string img_col_name = "img_color_" + std::to_string(counter) + ".jpg";
-        cv::imwrite(img_col_name, cv_frame_color);
-        std::string img_depth_name = "img_depth_" + std::to_string(counter) + ".jpg";
-        cv::imwrite(img_depth_name, cv_frame_depth);
+        cv::imshow("win1", cv_frame_depth);
+        cv::waitKey(0);
+
+        //std::string img_col_name = "img_color_" + std::to_string(counter) + ".png";
+        //cv::imwrite(img_col_name, cv_frame_color);
+        //std::string img_depth_name = "img_depth_" + std::to_string(counter) + ".png";
+        //cv::imwrite(img_depth_name, cv_frame_depth);
     }
 
-    //cv::waitKey(0);
+    cv::waitKey(0);
     return EXIT_SUCCESS;
 }
 catch (const rs2::error & e)
